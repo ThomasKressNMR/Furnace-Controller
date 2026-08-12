5352dee76e15b4f0c96152299c703ccd9492ce79
Thomas Kress <zorlaki21@gmail.com>
2026-08-12 18:05:44 +0100

    removed bat file because windows = no

--- furnace_controller.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM Initialize and activate the conda environment, then change directory and run Streamlit
-call C:\ProgramData\miniconda3\Scripts\activate.bat furnace
-cd /d C:\Users\Forse\Documents\Temperature_logging\streamlit
-streamlit run streamlit_app.py
-pause
